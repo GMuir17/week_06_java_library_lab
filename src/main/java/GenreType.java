0d4abbdd7a14f80551d32d417de52c7c04bf0a89
jamos90 <jamos_98@hotmail.com>
2018-06-26 15:53:03 +0100

    enums added

---
+++ src/main/java/GenreType.java
@@ -0,0 +1,12 @@
+public enum GenreType {
+    SCIFI ("Scifi Novels"),
+    HORROR("Scary Novels"),
+    CRIME("Crime Novels");
+
+
+    public final String descirption;
+
+    GenreType(String descirption){
+        this.descirption = descirption;
+    }
+}
