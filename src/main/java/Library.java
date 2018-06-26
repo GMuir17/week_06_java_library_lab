5738066520d5144c620e52deb8311bcfd8b6c314
jamos90 <jamos_98@hotmail.com>
2018-06-26 15:31:16 +0100

    mvp done

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,12 +3,23 @@ import java.util.ArrayList;
 public class Library {
 
     private ArrayList<Book> collection;
+    private Integer capacity;
 
-    public Library() {
+    public Library(Integer capacity) {
         this.collection = new ArrayList<>();
+        this.capacity = capacity;
     }
 
     public int bookCount() {
         return this.collection.size();
     }
+
+    public void addBook(Book book) {
+        if (bookCount() < this.capacity)
+        this.collection.add(book);
+    }
+
+
+
 }
+
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,16 +6,32 @@ import static org.junit.Assert.assertEquals;
 public class LibraryTest {
 
     Library library;
+    Book book;
 
     @Before
     public void setUp() {
-        library = new Library();
+        library = new Library(2);
     }
 
     @Test
     public void collectionStartsAsEmpty() {
         assertEquals(0, library.bookCount());
     }
+
+    @Test
+    public void addBookToCollection(){
+       library.addBook(book);
+       assertEquals(1, library.bookCount());
+    }
+
+    @Test
+    public void checkCapacityFull(){
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        assertEquals(2, library.bookCount());
+    }
 }
+
 
 
